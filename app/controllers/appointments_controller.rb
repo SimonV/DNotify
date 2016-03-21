0e906a1d7cb0b522558bdd5912acb51da5fb171a
SimonV <SimonV6@gmail.com>
2016-03-21 16:06:49 +0200

    split appts to load from 2 apis

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,15 +3,28 @@ class AppointmentsController < ApplicationController
     start = Date.parse(params[:start])
     first_date = start.at_beginning_of_month.next_month
     last_date = start.at_end_of_month.next_month
-    #year = params[:year]
 
-    resp = [{title: 'test', start: '2016-01-01T18:00:00.000Z', end: '2016-01-01T19:00:00.000Z', id: 10}]
+    #TODO render events from model
+    resp = [{title: 'test',
+              start: '2016-01-01T18:00:00.000Z',
+              #end: '2016-01-01T19:00:00.000Z',
+              allDay: true,
+              id: 10,
+              rendering: 'background',
+              color: '#257e4a'},
+            {title: 'test',
+              start: '2016-01-01T18:00:00.000Z',
+              #end: '2016-01-01T19:00:00.000Z',
+              id: 11,
+              color: '#FFAACC'}]
     render json: resp
   end
 
   def get_free_slots
+    #TODO render events from model
   end
 
   def list_appts
+    #TODO render events from model
   end
 end
