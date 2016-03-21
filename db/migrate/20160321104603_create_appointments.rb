6573471a71a2a3dfd14ba4893fd6c097639af01b
SimonV <SimonV6@gmail.com>
2016-03-21 12:46:41 +0200

    add appointment model

---
+++ db/migrate/20160321104603_create_appointments.rb
@@ -0,0 +1,15 @@
+class CreateAppointments < ActiveRecord::Migration
+  def change
+    create_table :appointments do |t|
+      t.references :doctor, index: true, foreign_key: true
+      t.references :customer, index: true, foreign_key: true
+      t.datetime :start_time
+      t.integer :dutation
+      t.string :description
+      t.string :status
+      t.datetime :status_change_time
+
+      t.timestamps null: false
+    end
+  end
+end
