ea9458dc2c82f1022da26ade5e9af5e9d51bb75b
David Aslanov <David14741@gmail.com>
2016-04-10 17:54:37 +0300

    #dev task 1- JS
    form is being auto filled in 2 areas (date and time)
    send button added and got its ajax function by the name
    createAppointment

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -6,23 +6,48 @@ $(document).ready(function() {
     height: 600,
     width: 400,
     modal: true,
-    buttons: {
-      "Create appointment": null,
-      /*
-      Cancel: function() {
-        dialog.dialog( "close" );
+    buttons: [{
+      text: "Send",
+      click: function() {
+        createAppointment();
       }
-      */
-    },
+    }],
     close: function() {
       form[ 0 ].reset();
     }
   });
 
+	//do we even need this?
   form = dialog.find( "form" ).on( "submit", function( event ) {
     event.preventDefault();
     //addUser();
   });
+
+	//button pressed
+	function createAppointment(){
+		var formData = {
+			'appt_date': $('#appt_date').val(),
+			'appt_time': $('#appt_time').val(),
+			'appt_duration': $('#appt_duration').val(),
+			'appt_description': $('#appt_description').val(),
+			'appt_customer_name': $('#appt_customer_name').val(),
+			'appt_customer_last_name': $('#appt_customer_last_name').val(),
+			'appt_customer_phone': $('#appt_customer_phone').val(),
+			'appt_customer_email': $('#appt_customer_email').val()
+			};
+			//alert for checking values
+			//alert(formData.appt_date +" "+ formData.appt_time +" "+formData.appt_duration +" "+formData.appt_description +" "+formData.appt_customer_name +" "+formData.appt_customer_last_name +" "+formData.appt_customer_phone +" "+formData.appt_customer_email);
+			
+		$.ajax({
+              url: 'appointments/create',
+              type: "POST",
+              data: formData,
+              success: function(doc) {
+                  var events = doc;
+                  callback(events);
+              }
+          });
+	}
 
   //$("#appt_close").click(function() {
   //  $("#appointment_form").css("display", "none");
@@ -49,6 +74,10 @@ $(document).ready(function() {
         //TODO add appointment
         //alert('add appt');
         //$("#appointment_form").css("display", "block");
+
+	   	$('#appt_date').val(date.format("YYYY-MM-DD"));
+		$('#appt_time').val(date.format("hh:mm"));
+		//$('#appt_time').val(date.format("hh:mm"));
         $( "#appointment_form" ).dialog( "open" );
       }
     },
