93c9aa30d2d7f1d173632d903311220ed58538f9
SimonV <SimonV6@gmail.com>
2016-05-14 18:32:06 +0300

    add basic field structure to customer page

---
+++ public/js/customer.js
@@ -0,0 +1,12 @@
+function searchForCustomers(searchString){
+  //TODO ajax to find
+  // display result list "search_results"
+}
+
+function showCustomer(id){
+  //TODO ajax get customer data when search result item is clicked, load in fields
+}
+
+function updateCustomer(id, data){
+  //TODO ajax update
+}
